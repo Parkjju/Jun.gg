8db9314b720d675ae5ab7ef2fd62b786dc707ae1
Parkjju <rudwns3927@gmail.com>
2022-04-06 00:02:07 +0900

    [Feature] 인풋박스 submit 이벤트추가, preventDefault 및 fetch데이터 로직 처리 구현 이전 // 데이터 localStorage에 저장 // window.location.href 프로퍼티 수정으로 페이지 리다이렉팅

--- api.js
+++ api.js
@@ -1,8 +1,21 @@
 const riotURL = 'https://kr.api.riotgames.com';
 const api = '/lol/summoner/v4/summoners/by-name/';
-const userName = encodeURI('손흥민은거품이다');
 const apiKey = JSON.parse(data).key;
+const inputBox = document.querySelector('#main-input');
+const inputUsernameBox = document.querySelector('#main-input__username');
 
-axios.get(`${riotURL}${api}${userName}?api_key=${apiKey}`).then((response) => {
-  console.log(response.data);
-});
+const submitHandler = (e) => {
+  e.preventDefault();
+  const userName = encodeURI(`${inputUsernameBox.value}`);
+  axios
+    .get(`${riotURL}${api}${userName}?api_key=${apiKey}`)
+    .then((response) => {
+      localStorage.setItem(
+        JSON.stringify(response.data.name),
+        JSON.stringify(response.data)
+      );
+    });
+  //   window.location.href = window.location.host + '/result.html';
+};
+
+inputBox.addEventListener('submit', submitHandler);
