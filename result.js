2b260b966e32e111e24af1b58eb8ec1c031bed68
Parkjju <rudwns3927@gmail.com>
2022-04-06 14:31:40 +0900

    [Add] 결과창 애셋파일 추가

---
+++ result.js
@@ -0,0 +1,3 @@
+const puuid = JSON.parse(localStorage.getItem(`"손흥민은거품이다"`));
+
+// {"id":"sTpanNWwHIm4fY0BP86pwWFySxXLoMgiGRSZuGA1Cxll8tY","accountId":"TtqxInE-D9l2y1ggNHrfBPZ_lAEtfNUPPfJLsX_AXrPpZR4","puuid":"mzZqAS3wmoPXH33ZFy6clYN-eG5HPj9WGSXHo6eoX6E3YnYW-hTq0ITLU7wOXx25aw13uX7SagYKAQ","name":"손흥민은거품이다","profileIconId":5145,"revisionDate":1648814132000,"summonerLevel":185}
