ab7cd284f924851346fc69652a5cdee25fae4e3a
Parkjju <rudwns3927@gmail.com>
2022-04-06 14:31:16 +0900

    [Fix] redirect 전 로컬스토리지에 데이터 저장 후 이동

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const api = '/lol/summoner/v4/summoners/by-name/';
 const apiKey = JSON.parse(data).key;
 const inputBox = document.querySelector('#main-input');
 const inputUsernameBox = document.querySelector('#main-input__username');
+const keyList = [];
 
 const submitHandler = (e) => {
   e.preventDefault();
@@ -14,8 +15,8 @@ const submitHandler = (e) => {
         JSON.stringify(response.data.name),
         JSON.stringify(response.data)
       );
+      window.location.href = '/result.html';
     });
-  //   window.location.href = window.location.host + '/result.html';
 };
 
 inputBox.addEventListener('submit', submitHandler);
