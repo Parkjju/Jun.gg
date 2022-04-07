29d35c1e603b33149b702ae69697b74512b5c628
Parkjju <rudwns3927@gmail.com>
2022-04-07 20:38:18 +0900

    [Fix] JSON parse메서드 삭제

--- api.js
+++ api.js
@@ -1,21 +1,21 @@
 const riotURL = 'https://kr.api.riotgames.com';
 const api = '/lol/summoner/v4/summoners/by-name/';
-const apiKey = JSON.parse(data).key;
+const apiKey = data.key;
 const inputBox = document.querySelector('#main-input');
 const inputUsernameBox = document.querySelector('#main-input__username');
 
 const submitHandler = (e) => {
-  e.preventDefault();
-  const userName = encodeURI(`${inputUsernameBox.value}`);
-  axios
-    .get(`${riotURL}${api}${userName}?api_key=${apiKey}`)
-    .then((response) => {
-      localStorage.setItem(
-        JSON.stringify(response.data.name),
-        JSON.stringify(response.data)
-      );
-      window.location.href = '/result.html';
-    });
+    e.preventDefault();
+    const userName = encodeURI(`${inputUsernameBox.value}`);
+    axios
+        .get(`${riotURL}${api}${userName}?api_key=${apiKey}`)
+        .then((response) => {
+            localStorage.setItem(
+                JSON.stringify(response.data.name),
+                JSON.stringify(response.data)
+            );
+            window.location.href = '/result.html';
+        });
 };
 
 inputBox.addEventListener('submit', submitHandler);
